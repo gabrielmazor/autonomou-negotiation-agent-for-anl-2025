5de99aa1ef70d1c891f5c4bf27d431266f1a9321
“Gabriel <gabimazor@gmail.com>
2025-02-18 18:39:03 +0200

    running evaluation tests

--- group4/group4.py
+++ group4/group4.py
@@ -287,4 +287,4 @@ def aspiration_function(t, mx, rv, e):
 if __name__ == "__main__":
     from .helpers.runner import run_a_tournament
 
-    run_a_tournament(Group4, small=False, debug=True)
+    run_a_tournament(Group4, small=True, debug=True)
